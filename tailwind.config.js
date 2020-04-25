ae746734945622c42f4b1e99c48a821dcee6631f
Christopher Tse <chris@chris-tse.com>
2020-04-25 17:12:08 -0500

    Add tailwind to project, set up npm stuffs

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        'fira': ['Fira Mono', 'monospace']
+      },
+      fontSize: {
+        '7xl': '5rem'
+      },
+      width: {
+        '30': '30%'
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
