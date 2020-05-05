7831168cda6909359e2e321e89f3e93598147739
Christopher Tse <chris@chris-tse.com>
2020-05-04 22:15:19 -0500

    Prettier formatting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  theme: {
-    extend: {
-      fontFamily: {
-        'fira': ['Fira Mono', 'monospace']
-      },
-      fontSize: {
-        '7xl': '5rem'
-      },
-      width: {
-        '30': '30%'
-      }
+    theme: {
+        extend: {
+            fontFamily: {
+                fira: ['Fira Mono', 'monospace'],
+            },
+            fontSize: {
+                '7xl': '5rem',
+            },
+            width: {
+                '30': '30%',
+            },
+        },
     },
-  },
-  variants: {},
-  plugins: [],
+    variants: {},
+    plugins: [],
 }
