7a36d00ed054a1346223933ff094fafd7d26156c
Christopher Tse <chris@chris-tse.com>
2020-05-04 22:20:22 -0500

    Add customizable music name

--- src/js/techlahoma-welcome-query.js
+++ src/js/techlahoma-welcome-query.js
@@ -1,6 +1,7 @@
 const params = new URLSearchParams(window.location.search)
 const group = params.get('group')
 const showMusic = params.get('showMusic') === 'on'
+const musicText = params.get('musicText')
 
 if (group) {
     document.querySelector('#group-name').textContent = group
@@ -8,4 +9,8 @@ if (group) {
 
 if (showMusic) {
     document.querySelector('#music-text').classList.remove('hidden')
+}
+
+if (musicText) {
+    document.querySelector('#music-name').textContent = musicText
 }
